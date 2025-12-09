7e1fb81eb5974a944a5f43c29357e16281e69b7f
(Shivam Upadhyay) <[shivamupadhyayshivam30@gmail.com]>
2025-12-09 08:37:11 +0530

    update code

---
+++ DBT/backend/test-server.js
@@ -0,0 +1,10 @@
+﻿const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Server is working!' }));
+});
+
+server.listen(4000, () => {
+    console.log('Test server running on port 4000');
+});
